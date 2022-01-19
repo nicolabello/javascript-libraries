31e1fa48073b48e35a91504883fbc50a077b00dd
Nicola Bello <nicola.bello.3@gmail.com>
2022-01-19 14:46:11 +0000

    fixed tests

--- libs/ng-router-history/src/lib/services/router-history.service.spec.ts
+++ libs/ng-router-history/src/lib/services/router-history.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
-
 import { RouterHistoryService } from './router-history.service';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('RouterHistoryService', () => {
   let service: RouterHistoryService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [RouterHistoryService],
+      imports: [RouterTestingModule],
+    });
     service = TestBed.inject(RouterHistoryService);
   });
 
