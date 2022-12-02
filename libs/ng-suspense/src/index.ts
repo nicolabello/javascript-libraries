22dbc503acf885c31d4661886b6cebcc1fba3b3f
Nicola Bello <nicola.bello@tide.co>
2022-12-02 11:26:07 +0000

    exposed suspense directive in index.ts

--- libs/ng-suspense/src/index.ts
+++ libs/ng-suspense/src/index.ts
@@ -1,4 +1,5 @@
 export * from './lib/components/suspense.component';
+export * from './lib/directives/suspense.directive';
 export * from './lib/directives/suspense-if-data.directive';
 export * from './lib/directives/suspense-if-empty.directive';
 export * from './lib/directives/suspense-if-error.directive';
